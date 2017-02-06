1031e9fbdb4f7ac56dc5da0e864130622c557cb5
bmulobi <bmulobi@gmail.com>
2017-02-07 00:37:35 +0300

    added function test_output_is_prime_only

--- test_cases.py
+++ test_cases.py
@@ -11,6 +11,10 @@ class ten_test_cases(unittest.TestCase):
     # test that string arguments are not allowed
     def test_no_strings_allowed(self):
         self.assertEqual(prime_numbers_generator("astring"),"String arguments are not allowed")
+        
+   # test output has prime numbers only    
+    def test_output_is_prime_only(self):
+        self.assertTrue(prime_output_checker(prime_numbers_generator(100)))     
             
             
 if __name__ == '__main__':      
